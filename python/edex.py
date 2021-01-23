3a4f2a715f9e42d2f4de17c4c557fea7ca46d741
Isaac Mills <rooster0055@gmail.com>
2021-01-23 13:06:46 -0500

    Add options support

--- python/edex.py
+++ python/edex.py
@@ -6,15 +6,32 @@ query="data"
 json_file = open('data/hist.json')
 
 print(sys.argv)
+
 data = json.load(json_file)
-for i in range(1, len(sys.argv)):
+if sys.argv[-1].startswith("-"):
     for j in range(eval("len("+query+")")):
-        if eval(query+"[j]['command'] == sys.argv[i]"):
-            query+="["+str(j)+"]['sub']"
+        if eval(query+"[j]['command'] == sys.argv[1]"):
+            query+="["+str(j)+"]['opts']"
             print(j)
             break
-if 
-exec(query+".append({\'command\':\'"+sys.argv[-1]+"\', \'sub\':[]})")
+    exec(query+".append(\'"+sys.argv[-1]+"\')")
+else:
+    try:
+        for i in range(1, len(sys.argv)):
+            for j in range(eval("len("+query+")")):
+                if eval(query+"[j]['command'] == sys.argv[i]"):
+                    query+="["+str(j)+"]['sub']"
+                    print(j)
+                    break
+        if len(sys.argv) <= 2:
+            exec(query+".append({\'command\':\'"+sys.argv[-1]+"\', \'sub\':[], \'opts\':[]})")
+        else:
+            exec(query+".append({\'command\':\'"+sys.argv[-1]+"\', \'sub\':[]})")
+    except KeyError:
+        query="data"
+        exec(query+".append({\'command\':\'"+sys.argv[-1]+"\', \'sub\':[], \'opts\':[]})")
+        del data[0]
+
 json_file.close()
 with open('data/hist.json', 'w') as outfile:
     json.dump(data, outfile)
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,23 @@ use std::{io::*, process::Command};
 use std::fs;
 #[derive(Deserialize, Serialize, Default, Debug, Clone)]
 struct CommandHist {
+    #[serde(default)]
     command: String,
+    #[serde(default)]
     sub: Vec<CommandHist>
+}
+#[derive(Deserialize, Serialize, Default, Debug, Clone)]
+struct CommandRoot {
+    #[serde(default)]
+    command: String,
+    #[serde(default)]
+    sub: Vec<CommandHist>,
+    #[serde(default)]
+    opts: Vec<String>
 }
 
 fn main(){
-    let mut command_hist: Vec<CommandHist> = serde_json::from_str(&fs::read_to_string("data/hist.json").unwrap()).unwrap();
+    let mut command_hist: Vec<CommandRoot> = serde_json::from_str(&fs::read_to_string("data/hist.json").unwrap()).unwrap();
     loop {
         print!("> ");
         stdout().flush().unwrap();
@@ -22,35 +33,52 @@ fn main(){
         let args: Vec<&str> = parts.collect();
         let depth = args.len();
         let mut current_index = command_hist.iter().position(|r| r.command == command);
-        let mut indexes: Vec<usize> = vec![current_index.unwrap()];
-        let mut endhist = command_hist[current_index.unwrap()].sub.clone();
-        for i in 0..depth {
-            current_index = endhist.iter().position(|r| r.command == args[i]);
-            match current_index {
-                Some(x) => {
-                    endhist = endhist[x].sub.clone();
-                    indexes.push(x)
-                }
-                None => {
-                    Command::new("python3")
-                        .arg("./python/edex.py")
-                        .arg(&command)
-                        .args(&args[0..i+1])
-                        .status()
-                        .expect("Python script failed");
-                    command_hist = serde_json::from_str(&fs::read_to_string("data/hist.json").unwrap()).unwrap();
+        if current_index != None {
+            let mut indexes: Vec<usize> = vec![current_index.unwrap()];
+            let mut endhist = command_hist[current_index.unwrap()].sub.clone();
+            for i in 0..depth {
+                current_index = endhist.iter().position(|r| r.command == args[i]);
+                match current_index {
+                    Some(x) => {
+                        endhist = endhist[x].sub.clone();
+                        indexes.push(x)
+                    }
+                    None => {
+                        Command::new("python3")
+                            .arg("./python/edex.py")
+                            .arg(&command)
+                            .args(&args[0..i+1])
+                            .status()
+                            .expect("Python script failed");
+                        command_hist = serde_json::from_str(&fs::read_to_string("data/hist.json").unwrap()).unwrap();
+                    }
                 }
             }
-        }
-        match current_index {
-            Some(_)  => {
-                let mut possible: Vec<String> = Vec::new();
-                for i in endhist.iter() {
-                    possible.push(i.command.clone());
-                }
-                println!("{:?}", possible);
-            },
-            None => ()
+            match current_index {
+                Some(_)  => {
+                    let mut possible: Vec<String> = Vec::new();
+                    for i in endhist.iter() {
+                        possible.push(i.command.clone());
+                    }
+                    println!("{:?}", possible);
+                },
+                None => ()
+            }
+        } else {
+            Command::new("python3")
+                .arg("./python/edex.py")
+                .arg(&command)
+                .status()
+                .expect("Python script failed");
+            for i in 0..depth {
+                Command::new("python3")
+                    .arg("./python/edex.py")
+                    .arg(&command)
+                    .args(&args[0..i+1])
+                    .status()
+                    .expect("Python script failed");
+            }
+            command_hist = serde_json::from_str(&fs::read_to_string("data/hist.json").unwrap()).unwrap();
         }
     }
 }
